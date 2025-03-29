e17babd72e00b9e8d1f44a7ef5316176fc5aa1c4
Viktor Persson <viktor.persson@arcsin.se>
2025-03-30 00:01:58 +0100

    Breaking change: Use UdpSocket from tokio for async functions.

--- src/lib.rs
+++ src/lib.rs
@@ -1,89 +1,116 @@
 //! # tinyudp
 //! A tiny abstraction for UDP.
-use std::net::{Ipv4Addr, SocketAddrV4, ToSocketAddrs, UdpSocket};
+use anyhow::Result;
+use std::net::{Ipv4Addr, SocketAddrV4};
 use std::time::Duration;
+use thiserror::Error;
+use tokio::net::{ToSocketAddrs, UdpSocket};
 
-use anyhow::{anyhow as e, Result};
+#[derive(Debug, Error)]
+pub enum TinyudpError {
+    #[error("failed to bind socket: {0}")]
+    BindFailed(#[source] std::io::Error),
 
-pub struct ReadOptions {
-    pub timeout: Option<Duration>,
-    pub buffer_size: usize,
-}
+    #[error("failed to send message: {0}")]
+    SendFailed(#[source] std::io::Error),
 
-impl Default for ReadOptions {
-    fn default() -> Self {
-        ReadOptions {
-            timeout: None,
-            buffer_size: 32 * 1024, // 32 kb
-        }
-    }
-}
+    #[error("failed to receive message: {0}")]
+    ReceiveFailed(#[source] std::io::Error),
 
-/// A UDP client that reuses the same socket for all operations.
-pub struct Client {
-    socket: UdpSocket,
-}
-
-impl Client {
-    /// Create a new UDP client bound to the specified address.
-    pub fn new(bind_address: impl ToSocketAddrs) -> Result<Self> {
-        let socket = UdpSocket::bind(bind_address).map_err(|e| e!("tinyudp::bind: {}", e))?;
-        Ok(Self { socket })
-    }
-
-    /// Create a new UDP client bound to an unspecified address.
-    pub fn new_unspecified() -> Result<Self> {
-        Self::new(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, 0))
-    }
-
-    /// Send a message to the specified address.
-    fn send(&self, address: impl ToSocketAddrs, message: &[u8]) -> Result<usize> {
-        self.socket
-            .send_to(message, address)
-            .map_err(|e| e!("tinyudp::send: {}", e))
-    }
-
-    /// Read a message from the specified address.
-    fn read(&self, address: impl ToSocketAddrs, options: &ReadOptions) -> Result<Vec<u8>> {
-        self.socket.connect(address)?;
-        self.socket.set_read_timeout(options.timeout)?;
-        let mut buffer: Vec<u8> = vec![0; options.buffer_size];
-        let bytes_read = self
-            .socket
-            .recv(&mut buffer)
-            .map_err(|e| e!("tinyudp::read: {}", e))?;
-
-        let response = &buffer[..bytes_read];
-
-        Ok(Vec::from(response))
-    }
-
-    /// Send a message to the specified address and read the response.
-    fn send_and_read(
-        &self,
-        address: impl ToSocketAddrs,
-        message: &[u8],
-        read_options: &ReadOptions,
-    ) -> Result<Vec<u8>> {
-        self.send(&address, message)?;
-        self.read(&address, read_options)
-    }
+    #[error("timeout reached while waiting for response")]
+    TimeoutReached,
 }
 
 /// Send a message to the specified address.
-pub fn send(address: impl ToSocketAddrs, message: &[u8]) -> Result<usize> {
-    Client::new_unspecified()?.send(address, message)
+pub async fn send(target: impl ToSocketAddrs, message: &[u8]) -> Result<(), TinyudpError> {
+    bind()
+        .await?
+        .send_to(message, target)
+        .await
+        .map_err(TinyudpError::SendFailed)?;
+    Ok(())
 }
 
-/// Read a message from the specified address.
-pub fn read(address: impl ToSocketAddrs, options: &ReadOptions) -> Result<Vec<u8>> {
-    Client::new_unspecified()?.read(address, options)
-}
-
-pub fn send_and_read(
-    address: impl ToSocketAddrs,
+/// Send a message to the specified address and read the response.
+pub async fn send_and_receive(
+    target: impl ToSocketAddrs,
     message: &[u8],
-    read_options: &ReadOptions,
-) -> Result<Vec<u8>> {
-    Client::new_unspecified()?.send_and_read(address, message, read_options)
+    options: ReadOptions,
+) -> Result<Vec<u8>, TinyudpError> {
+    let socket = bind().await?;
+    socket
+        .send_to(message, target)
+        .await
+        .map_err(TinyudpError::SendFailed)?;
+
+    let mut buffer = vec![0; options.buffer_size];
+    let (bytes_read, _) = tokio::select! {
+        _ = tokio::time::sleep(options.timeout) => Err(TinyudpError::TimeoutReached),
+        res = socket.recv_from(&mut buffer) => res.map_err(TinyudpError::ReceiveFailed),
+    }?;
+
+    let response = buffer[..bytes_read].to_vec();
+    Ok(response)
+}
+
+async fn bind() -> Result<UdpSocket, TinyudpError> {
+    let address = SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, 0);
+    UdpSocket::bind(address)
+        .await
+        .map_err(TinyudpError::BindFailed)
+}
+
+#[derive(Debug)]
+pub struct ReadOptions {
+    pub timeout: Duration,
+    pub buffer_size: usize,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use anyhow::Result;
+
+    #[tokio::test]
+    #[cfg_attr(feature = "ci", ignore)]
+    async fn test_send() -> Result<()> {
+        let res = send("quake.se:28501", b"\xff\xff\xff\xffstatus").await;
+        assert!(res.is_ok());
+        Ok(())
+    }
+
+    #[tokio::test]
+    #[cfg_attr(feature = "ci", ignore)]
+    async fn test_send_and_receive() -> Result<()> {
+        {
+            // timeout reached
+            let response = send_and_receive(
+                "quake.se:28501",
+                b"\xff\xff\xff\xffstatus",
+                ReadOptions {
+                    timeout: Duration::from_secs_f32(0.001),
+                    buffer_size: 32 * 1024,
+                },
+            )
+            .await;
+            assert_eq!(
+                response.unwrap_err().to_string(),
+                "timeout reached while waiting for response"
+            );
+        }
+        {
+            // ok
+            let response = send_and_receive(
+                "quake.se:28501",
+                b"\xff\xff\xff\xffstatus",
+                ReadOptions {
+                    timeout: Duration::from_secs_f32(0.2),
+                    buffer_size: 32 * 1024,
+                },
+            )
+            .await?;
+            assert!(String::from_utf8_lossy(&response).contains("QUAKE.SE KTX"));
+        }
+        Ok(())
+    }
 }
